ac8254e8629462c35609820df2a76d97e8ad840c
Florian Dupuis <floriandupuis1996@gmail.com>
2022-02-28 16:55:24 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth',
+        destination:
+          'https://loginval.aeroportsdeparis.fr/Adp.IdentityFederationV4.web/api/User/get-connected-user',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

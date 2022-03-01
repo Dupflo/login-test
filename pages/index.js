6a098e5a281087e4adc1cc1bedfdb0b459cbff1e
Florian Dupuis <floriandupuis1996@gmail.com>
2022-03-01 19:09:57 +0100

    Add login portal

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ const headers = {
 // const urlToTest =
 //   'https://loginval.aeroportsdeparis.fr/Adp.IdentityFederationV4.web/api/User/get-connected-user'
 
-const urlToTestRewrite = 'http://localhost:3000/api/is-user-connected' //check next.config.js
+const urlToTestRewrite =
+  'https://msdn-frc-app-customers.azurewebsites.net/api/is-user-connected' //check next.config.js
 
 const url = urlToTestRewrite // add urlToTestRewrite or urlToTest
 
